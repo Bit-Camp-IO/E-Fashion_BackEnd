7ffcc539332aacc647bdb415157a5b1fbc63c653
M0Leo <mohamedleo404@gmail.com>
2023-08-13 23:57:08 +0300

    fix

--- src/database/models/discount.ts
+++ src/database/models/discount.ts
@@ -2,13 +2,13 @@ import mongoose, { Schema } from 'mongoose';
 import { ObjectId, RelationList } from '@type/database';
 import { CategorieDB } from './categorie';
 import { BrandDB } from './brand';
-import { ProductDB } from './product';
+import  { ProductDB } from './product';
 
 export interface DiscountDB {
   categories?: RelationList<CategorieDB>;
   brands?: RelationList<BrandDB>;
   products?: RelationList<ProductDB>;
-  discount: number;
+  percentage: number;
   startDate: Number;
   endDate: Number;
 }
@@ -17,7 +17,7 @@ const discountSchema = new Schema<DiscountDB>({
   products: [{ type: ObjectId, ref: 'Product' }],
   categories: [{ type: ObjectId, ref: 'Category' }],
   brands: [{ type: ObjectId, ref: 'Brand' }],
-  discount: { type: Number, required: true },
+  percentage: { type: Number, required: true },
   startDate: { type: Number, required: true, default: Date.now },
   endDate: { type: Number, required: true }
 });
