9661f41a1ef7de583bc1f3208342a21257a8fa93
M0Leo <mohamedleo404@gmail.com>
2023-08-13 23:20:14 +0300

    fix typo

--- src/database/models/discount.ts
+++ src/database/models/discount.ts
@@ -4,21 +4,21 @@ import { CategorieDB } from './categorie';
 import { BrandDB } from './brand';
 import { ProductDB } from './product';
 
-export interface ReviewDB {
+export interface DiscountDB {
   category?: Relation<CategorieDB>;
   brand?: Relation<BrandDB>;
-  product: Relation<ProductDB>
+  product?: Relation<ProductDB>;
   discount: number;
 }
 
-const discountSchema = new Schema<ReviewDB>(
-  {
-    product: { type: ObjectId, ref: 'Product', required: true },
-    category: { type: ObjectId, ref: 'Category' },
-    brand: { type: ObjectId, ref: 'Brand' },
-    discount: { type: Number, required: true }
-  }
-)
+const discountSchema = new Schema<DiscountDB>({
+  product: { type: ObjectId, ref: 'Product' },
+  category: { type: ObjectId, ref: 'Category' },
+  brand: { type: ObjectId, ref: 'Brand' },
+  discount: { type: Number, required: true },
+});
+
+discountSchema.index({ product: 1, category: 1, brand: 1 }, { unique: true });
 
 const DiscountModel = mongoose.model('Discount', discountSchema);
 export default DiscountModel;
