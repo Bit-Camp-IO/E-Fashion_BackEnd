a5cb3ea5f8c4f1cb42fe5599d7fb38381c5cdb8f
M0Leo <mohamedleo404@gmail.com>
2023-08-29 23:06:33 +0300

    fix comment min max validation

--- src/server/api/product/product.valid.ts
+++ src/server/api/product/product.valid.ts
@@ -7,5 +7,5 @@ export interface ReviewSchema {
 
 export const reviewSchema = Joi.object({
   rate: Joi.number().required().min(1).max(5),
-  comment: Joi.string().required(),
+  comment: Joi.string().required().min(50).max(500),
 });
