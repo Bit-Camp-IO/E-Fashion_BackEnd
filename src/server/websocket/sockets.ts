afb3d87bd0c79cc04287e1e482b4dad3e6fc2e77
M0Leo <mohamedleo404@gmail.com>
2023-09-08 03:48:30 +0300

    sockets1

--- src/server/websocket/sockets.ts
+++ src/server/websocket/sockets.ts
@@ -4,14 +4,28 @@ import { Server, Socket } from "socket.io";
 export async function chatSocket(io: Server) {
   io.on('connection', (socket: Socket) => {
 
-    socket.on('adminJoin', async (chatId, adminId) => {
+    socket.on('admin-join', async (chatId, adminId) => {
       const chat = await ChatModel.findByIdAndUpdate(chatId, {
         $set: { admin: adminId, status: 'active' }
-      });
+      }, { new: true });
       if (!chat) {
         throw new Error("Something went wrong")
       }
-      socket.emit('adminJoined', chatId);
+
+      socket.to(chatId).emit('admin-joined', adminId)
+    })
+
+    socket.on('send-message', async (senderId, chatId, message) => {
+      await ChatModel.findByIdAndUpdate(chatId, {
+        $push: {
+          messages: {
+            sender: senderId,
+            content: message
+          }
+        }
+      }
+      )
+      socket.to(chatId).emit('recieve-message', senderId, message)
     })
   })
 }
