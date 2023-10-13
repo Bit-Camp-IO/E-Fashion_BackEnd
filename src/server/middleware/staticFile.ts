5ab10172e6af9a7848bcb23eb53deacd5d67cee2
Mahmoud-Khaled-FS <mahmoudkhaled1215@gmail.com>
2023-10-13 19:58:06 +0300

    fix static file

--- src/server/middleware/staticFile.ts
+++ src/server/middleware/staticFile.ts
@@ -2,5 +2,5 @@ import express from 'express';
 import { join } from 'path';
 
 export function staticFileMiddleware(app: express.Express) {
-  app.use('/api/u', express.static(join(__dirname, '..', '..', 'uploads')));
+  app.use('/api/u', express.static(join(__dirname, '..', '..', '..', 'uploads')));
 }
