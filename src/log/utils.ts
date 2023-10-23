f4d17d9b442cd292fa0666302bce03c3a33d3f7b
Mohamed Khaled <mohamedleo404@gmail.com>
2023-10-24 01:26:24 +0300

    edit logger format

--- src/log/utils.ts
+++ src/log/utils.ts
@@ -4,12 +4,12 @@ export function logRequest(method: string, path: string, ip: string, agent: stri
 
 export function logResponse(method: string, path: string, statusCode: number, ms: number) {
   if (statusCode >= 500) {
-    log.error(`[${method}] ${path} -> |${statusCode}| [${ms}ms]`);
+    log.error(`[${method}] ${path} -> ${statusCode} (${ms}ms)`);
     return;
   } else if (statusCode >= 400) {
-    log.warn(`[${method}] ${path} -> |${statusCode}| [${ms}ms]`);
+    log.warn(`[${method}] ${path} -> ${statusCode} (${ms}ms)`);
     return;
   } else {
-    log.info(`[${method}] ${path} -> |${statusCode}| [${ms}ms]`);
+    log.info(`[${method}] ${path} -> ${statusCode} (${ms}ms)`);
   }
 }
