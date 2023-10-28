0f9c29c8bf2fc7407ee7a9becfb410540a9e0df2
Mahmoud-Khaled-FS <mahmoudkhaled1215@gmail.com>
2023-10-28 22:22:43 +0200

    fix config

--- src/config/init.ts
+++ src/config/init.ts
@@ -6,7 +6,7 @@ import { CreateProDir } from './initDir';
 type Key = keyof typeof Config;
 
 for (const key in Config) {
-  if (!Config[key as Key] || Config[key as Key] === '') {
+  if (Config[key as Key] === undefined || Config[key as Key] === '') {
     throw new Error(`Environment variable '${key}' not found`);
   }
 }
