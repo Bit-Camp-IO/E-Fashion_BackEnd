1e6ebfd9829529c6271f0703d19636cbb195149c
Mahmoud-Khaled-FS <mahmoudkhaled1215@gmail.com>
2025-06-13 15:49:50 +0300

    replace encoded key with file .key file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,13 @@
+import * as fs from 'fs';
 import { join } from 'node:path';
 
 const Config = {
   PORT: process.env['PORT']!,
   MONGODB_URI: process.env['MONGODB_URI']!,
-  ACCESS_TOKEN_PRIVATE_KEY: process.env['ACCESS_TOKEN_PRIVATE_KEY']!,
-  ACCESS_TOKEN_PUBLIC_KEY: process.env['ACCESS_TOKEN_PUBLIC_KEY']!,
-  REFRESH_TOKEN_PRIVATE_KEY: process.env['REFRESH_TOKEN_PRIVATE_KEY']!,
-  REFRESH_TOKEN_PUBLIC_KEY: process.env['REFRESH_TOKEN_PUBLIC_KEY']!,
+  ACCESS_TOKEN_PRIVATE_KEY: fs.readFileSync(process.env['ACCESS_TOKEN_PRIVATE_KEY']!, 'utf-8'),
+  ACCESS_TOKEN_PUBLIC_KEY: fs.readFileSync(process.env['ACCESS_TOKEN_PUBLIC_KEY']!, 'utf-8'),
+  REFRESH_TOKEN_PRIVATE_KEY: fs.readFileSync(process.env['REFRESH_TOKEN_PRIVATE_KEY']!, 'utf-8'),
+  REFRESH_TOKEN_PUBLIC_KEY: fs.readFileSync(process.env['REFRESH_TOKEN_PUBLIC_KEY']!, 'utf-8'),
   REFRESH_TOKEN_EXP: process.env['REFRESH_TOKEN_EXP']!,
   ACCESS_TOKEN_EXP: process.env['ACCESS_TOKEN_EXP']!,
   GOOGLE_ID: process.env['GOOGLE_CLIENT_ID']!,
--- src/core/auth/token.ts
+++ src/core/auth/token.ts
@@ -2,8 +2,7 @@ import jwt from 'jsonwebtoken';
 import { AppError } from '../errors';
 
 export function createToken(payload: any, key: string, exp: string): string {
-  const keyEncoded = Buffer.from(key, 'base64').toString('utf-8');
-  return jwt.sign(payload, keyEncoded, {
+  return jwt.sign(payload, key, {
     algorithm: 'RS256',
     expiresIn: exp,
   });
@@ -11,8 +10,7 @@ export function createToken(payload: any, key: string, exp: string): string {
 
 export function verifyToken(token: string, key: string): any {
   try {
-    const keyEncoded = Buffer.from(key, 'base64').toString('utf-8');
-    return jwt.verify(token, keyEncoded, {
+    return jwt.verify(token, key, {
       ignoreExpiration: false,
       algorithms: ['RS256'],
     });
